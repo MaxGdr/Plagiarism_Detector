370e3869b92ce36fafef4bbb6551290e583bc021
Maxime GENDRE <gendre.maxime@gmail.com>
2021-01-11 16:44:30 +0100

    Define Pytorch neural network

--- source_pytorch/model.py
+++ source_pytorch/model.py
@@ -1,5 +1,5 @@
 # torch imports
-import torch.nn.functional as F
+from torch.nn.functional import sigmoid
 import torch.nn as nn
 
 
@@ -16,7 +16,7 @@ class BinaryClassifier(nn.Module):
     """
 
     ## TODO: Define the init function, the input params are required (for loading code in train.py to work)
-    def __init__(self, input_features, hidden_dim, output_dim):
+    def __init__(self, input_dim, hidden_dim, output_dim):
         """
         Initialize the model by setting up linear layers.
         Use the input parameters to help define the layers of your model.
@@ -25,13 +25,19 @@ class BinaryClassifier(nn.Module):
         :param output_dim: the number of outputs you want to produce
         """
         super(BinaryClassifier, self).__init__()
-
-        # define any initial layers, here
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
         
-
+        # define any initial layers, here      
+        self.input = nn.Linear(in_features=self.input_dim, out_features=self.hidden_dim)
+        self.relu_1 = nn.ReLU()
+        self.lin_1 = nn.Linear(in_features=self.hidden_dim, out_features=120)
+        self.prelu = nn.PReLU(1)
+        self.out = nn.Linear(in_features=120, out_features=self.output_dim)
     
     ## TODO: Define the feedforward behavior of the network
-    def forward(self, x):
+    def forward(self, input_):
         """
         Perform a forward pass of our model on input features, x.
         :param x: A batch of input features of size (batch_size, input_features)
@@ -39,6 +45,10 @@ class BinaryClassifier(nn.Module):
         """
         
         # define the feedforward behavior
-        
-        return x
+        x = self.input(input_)
+        x = self.relu_1(x)
+        x = self.lin_1(x)
+        x = self.prelu(x)
+        x = self.out(x)
+        return sigmoid(x)
     
